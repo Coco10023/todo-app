88a9b2a2f1e3df1864357340642629dede142f0e
Coco10023 <mauricio2003@live.se>
2026-04-17 23:53:54 +0200

    Börjar med att skriva kod för TodoList

--- src/model/TodoList.ts
+++ src/model/TodoList.ts
@@ -1,2 +1,12 @@
+import type { Todo, Priority } from "./Todo"; 
 
+export class TodoList {
+    private todos: Todo[]; 
+    private readonly storageKey: string = "todos"; 
+}
+
+constructor() {
+    this.todos = []; 
+    this.loadFromLocalStorage();
+}
 
